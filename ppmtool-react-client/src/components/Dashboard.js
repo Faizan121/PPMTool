5d2748b40d9717c19ba569cf24de87723267e71a
faizan uddin <faizan.uddin121@gmail.com>
2019-08-02 17:34:04 -0500

    first react component

---
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -0,0 +1,20 @@
+import react, { Component } from 'react';
+
+
+ class Dashboard extends Component  {
+
+    render () {
+
+        return (
+            <div>
+            <h1>Welocme to Dashboard!</h1>
+            </div>
+        )
+
+
+    }
+
+
+}
+
+export default Dashboard;
