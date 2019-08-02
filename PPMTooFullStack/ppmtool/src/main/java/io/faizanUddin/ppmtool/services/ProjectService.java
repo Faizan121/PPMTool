00f1a844c9c85859c433d54e936f0455eac4aed4
faizan uddin <faizan.uddin121@gmail.com>
2019-08-02 11:40:41 -0500

    Find Project by Identifier

--- PPMTooFullStack/ppmtool/src/main/java/io/faizanUddin/ppmtool/services/ProjectService.java
+++ PPMTooFullStack/ppmtool/src/main/java/io/faizanUddin/ppmtool/services/ProjectService.java
@@ -23,5 +23,18 @@ public class ProjectService {
         }
     }
 
+    public Project findProjectByIdentifier(String projectId ) {
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if (project == null){
+            throw new ProjectIdException("Project Identifier '" + projectId.toUpperCase() +"' does not exist");
+        }
+
+         return project;
+
+    }
+
+
 
 }
