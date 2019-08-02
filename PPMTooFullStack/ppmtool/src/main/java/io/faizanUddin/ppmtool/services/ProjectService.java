b7a5465b3f5c64a3f3fccdb0f1455ebb9fa3f21f
faizan uddin <faizan.uddin121@gmail.com>
2019-08-02 10:48:05 -0500

    Custom Exception for Unique Project handler

--- PPMTooFullStack/ppmtool/src/main/java/io/faizanUddin/ppmtool/services/ProjectService.java
+++ PPMTooFullStack/ppmtool/src/main/java/io/faizanUddin/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.faizanUddin.ppmtool.services;
 
 import io.faizanUddin.ppmtool.domain.Project;
+import io.faizanUddin.ppmtool.exceptions.ProjectIdException;
 import io.faizanUddin.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,13 @@ public class ProjectService {
 
     public Project saveOrUpdate (Project project) {
 
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }
+        catch (Exception e ){
+            throw new ProjectIdException("Project Identifier '" + project.getProjectIdentifier() +"' is already exist");
+        }
     }
 
 
