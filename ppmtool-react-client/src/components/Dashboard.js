fbc44a915e62b210735ddbdfec5043ea763fdbda
faizan uddin <faizan.uddin121@gmail.com>
2019-08-18 16:16:43 -0500

    get Projects redux-only

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import ProjectItem from './Project/ProjectItem';
 import CreateProjectButton from './Project/CreateProjectButton';
+import { connect } from 'react-redux';
+import {getProjects } from '../actions/projectActions';
+import PropTypes from 'prop-types';
 
 
 
  class Dashboard extends Component  {
+
+    componentDidMount () {
+        this.props.getProjects();
+    }
 
     render () {
         return (
@@ -27,4 +34,14 @@ import CreateProjectButton from './Project/CreateProjectButton';
     }
 }
 
-export default Dashboard;
+Dashboard.propTypes = {
+    project : PropTypes.object.isRequired,
+    getProjects : PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+    project : state.project
+});
+
+
+export default connect(mapStateToProps , {getProjects}) (Dashboard);
