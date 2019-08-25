71a93dca2209479622abf8a1770da6c02e135c31
faizan uddin <faizan.uddin121@gmail.com>
2019-08-25 13:49:31 -0500

    Persist Project Task(Bug fixing pending: setPriority)

---
+++ PPMTooFullStack/ppmtool/src/main/java/io/faizanUddin/ppmtool/services/ProjectTaskService.java
@@ -0,0 +1,40 @@
+package io.faizanUddin.ppmtool.services;
+
+
+import io.faizanUddin.ppmtool.domain.Backlog;
+import io.faizanUddin.ppmtool.domain.ProjectTask;
+import io.faizanUddin.ppmtool.repositories.BacklogRepository;
+import io.faizanUddin.ppmtool.repositories.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectTaskService {
+
+    @Autowired
+    private BacklogRepository backlogRepository;
+    @Autowired
+    private ProjectTaskRepository projectTaskRepository;
+
+
+    public ProjectTask addProjectTask (String projectIdentifier, ProjectTask projectTask){
+
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+
+        projectTask.setBacklog(backlog);
+
+        Integer backlogSequence = backlog.getPTSequence();
+        backlogSequence++;
+        projectTask.setProjectSequence(projectIdentifier+"-"+backlogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
+
+//        if (projectTask.getPriority() == 0 || projectTask.getPriority() == null){
+//            projectTask.setPriority(3);
+//        }
+        if (projectTask.getStatus() == null || projectTask.getStatus() == ""){
+            projectTask.setStatus("TO_DO");
+        }
+
+        return projectTaskRepository.save(projectTask);
+    }
+}
