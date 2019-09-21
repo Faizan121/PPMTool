2f2585fe9941fa262b212e35f8537118d3ac79d5
faizan uddin <faizan.uddin121@gmail.com>
2019-09-21 16:43:36 -0500

    UpdateProjectTask part 3

--- ppmtool-react-client/src/actions/backlogActions.js
+++ ppmtool-react-client/src/actions/backlogActions.js
@@ -54,3 +54,22 @@ export const getProjectTask = (backlog_id, pt_id, history) => async dispatch => 
     }
 
 }; 
+
+export const updateProjectTask = (backlog_id, pt_id, project_Task, history) => async dispatch => {
+
+    try {
+        const res = await axios.patch(`/api/backlog/${backlog_id}/${pt_id}`, project_Task);
+        history.push(`/projectBoard/${backlog_id}`);
+        dispatch({
+            type: GET_ERRORS,
+            payload: {}
+        });
+
+    } catch (err) {
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        })
+    }
+
+}
