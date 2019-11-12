494161c98c47a01ebf72d33697fb8f3f14802b34
faizan uddin <faizan.uddin121@gmail.com>
2019-11-12 13:05:41 -0600

    handle routing for expired token

--- ppmtool-react-client/src/components/UserManagement/Login.js
+++ ppmtool-react-client/src/components/UserManagement/Login.js
@@ -10,7 +10,8 @@ class Login extends Component {
         super()
         this.state = {
             "username": "",
-            "password": ""
+            "password": "",
+            "errors" : {}
         };
         this.onChange = this.onChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
@@ -21,6 +22,10 @@ class Login extends Component {
         if (nextProps.security.validToken){
             this.props.history.push("/Dashboard");
         }
+        if (nextProps.errors){
+            this.setState({errors:nextProps.errors});
+        }
+
     }
 
     onSubmit(e){
@@ -38,6 +43,8 @@ class Login extends Component {
 
 
     render() {
+
+        const {errors} = this.state;
         return (
             <div className="login">
         <div className="container">
@@ -46,16 +53,26 @@ class Login extends Component {
                     <h1 className="display-4 text-center">Log In</h1>
                     <form onSubmit = {this.onSubmit}>
                         <div className="form-group">
-                            <input type="text" className="form-control form-control-lg" 
+                            <input type="text" className={classnames("form-control form-control-lg", 
+                            { "is-invalid" : errors.username })} 
                             placeholder="Email Address" name="username" 
                             value = {this.state.username} 
                             onChange = {this.onChange} />
+                            {
+                                errors.username && ( <div className="invalid-feedback">{errors.username}</div> )
+                            }
                         </div>
                         <div className="form-group">
-                            <input type="password" className="form-control form-control-lg" 
+                            <input type="password" className={classnames("form-control form-control-lg", {
+                                "is-invalid" : errors.password
+                            })} 
                             placeholder="Password" name="password" 
                             value = {this.state.password} 
                             onChange = {this.onChange} />
+                            {
+                                errors.password && ( <div className="invalid-feedback">{errors.password}</div> )
+                            }
+
                         </div>
                         <input type="submit" className="btn btn-info btn-block mt-4" />
                     </form>
@@ -69,7 +86,8 @@ class Login extends Component {
 
 Login.propTypes = {
     login : PropTypes.func.isRequired,
-    errors : PropTypes.object.isRequired
+    errors : PropTypes.object.isRequired,
+    security : PropTypes.object.isRequired
 }
 
 const mapStateToProps = state => ({
